f13252a250e4d929f66f581233760e0bfd87d910
Illari Alvarez-Gil <friedapples824@gmail.com>
2021-01-27 19:50:31 -0500

    creating payment-done

--- BeautyCorp/src/app/app.module.ts
+++ BeautyCorp/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { ItemProductComponent } from './components/item-product/item-product.com
 import { PaymentsNextComponent } from './components/payments-next/payments-next.component';
 
 import { CardProductComponent } from './components/card-product/card-product.component';
+import { PaymentDoneComponent } from './components/payment-done/payment-done.component';
 
 
 @NgModule({
@@ -62,6 +63,8 @@ import { CardProductComponent } from './components/card-product/card-product.com
     PaymentsNextComponent,
 
     CardProductComponent,
+
+    PaymentDoneComponent,
 
   ],
   imports: [
