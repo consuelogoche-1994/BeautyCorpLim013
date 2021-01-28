c7900445473a7fbce4f2e7a027f19493de21d896
SinaisAna <anaguanda20@gmail.com>
2021-01-27 22:11:48 -0500

    carrito de compras listo

--- BeautyCorp/src/app/components/navbar-esika/navbar-esika.component.ts
+++ BeautyCorp/src/app/components/navbar-esika/navbar-esika.component.ts
@@ -6,32 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-esika.component.scss']
 })
 export class NavbarEsikaComponent implements OnInit {
-  isScrollActive:boolean=false;
-// activar scroll al bajar pantalla
-  scroll(event){
-    if(window.scrollY>50){
-      this.isScrollActive=true;
-    }else{
-      this.isScrollActive=false;
+  isScrollActive: boolean = false;
+  // activar scroll al bajar pantalla
+  scroll(event) {
+    if (window.scrollY > 50) {
+      this.isScrollActive = true;
+    } else {
+      this.isScrollActive = false;
     }
   }
-  public products:any;
+  public products: any;
   constructor() { }
   ngOnInit(): void {
+
+  }
+  cart() {
+    //alert("sora");
     const productsLocal = JSON.parse(localStorage.getItem("products"));
-    this.products = productsLocal.products;
+    if (productsLocal) { this.products = productsLocal.products; }
+
+    if (document.querySelector("#shopping-cart.hidden")) {
+      document.querySelector("#shopping-cart").classList.remove("hidden");
+    } else {
+      document.querySelector("#shopping-cart").classList.add("hidden");
+    }
   }
-cart(){
-  //alert("sora");
-  
-  if(document.querySelector("#shopping-cart.hidden")){
-    document.querySelector("#shopping-cart").classList.remove("hidden");
-  }else{
-    document.querySelector("#shopping-cart").classList.add("hidden");
+  exit(id) {
+    // document.querySelector(".item-cart").classList.add("hidden");
+    const productsLocal = JSON.parse(localStorage.getItem("products"));
+    let arrProducts = productsLocal.products;
+    console.log("id",id,arrProducts);
+    for (let e = 0; e < arrProducts.length; e++) {
+      if (arrProducts[e].id == id) {
+        console.log("hola entre");
+        arrProducts.splice(e, 1);
+        break;
+      }
+    }
+    productsLocal.products=arrProducts;
+    localStorage.setItem("products",JSON.stringify(productsLocal));
+    //this.cart();
+
   }
-}
-exit(){
-  console.log("hola");
-  document.querySelector(".item-cart").classList.add("hidden");
-}
 }
