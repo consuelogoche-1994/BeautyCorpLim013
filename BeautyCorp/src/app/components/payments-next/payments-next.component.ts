a773d2503caaf8e5c1274ecb32f55e07cb2f1e11
consuelogoche-1994 <consuelo.goche@gmail.com>
2021-01-28 05:53:18 -0500

    deploy

--- BeautyCorp/src/app/components/payments-next/payments-next.component.ts
+++ BeautyCorp/src/app/components/payments-next/payments-next.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-payments-next',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payments-next.component.scss']
 })
 export class PaymentsNextComponent implements OnInit {
+  orderDetail:any;
+  deleteProduct(_index,detail){
+    this.orderDetail.splice(_index, 1);
+  }
 
-  constructor() { }
+  constructor(private data:DataService) { }
 
   ngOnInit(): void {
+    this.data.currentOrderDetail.subscribe(order => this.orderDetail=order);
   }
 
 }
